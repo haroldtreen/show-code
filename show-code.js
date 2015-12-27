60ed1a7a7d9d78c8a12cd01710ef485aa0caab33
Harold Treen <haroldtreen@gmail.com>
2015-12-27 16:53:03 -0500

    Working demo

--- show-code.js
+++ show-code.js
@@ -3,13 +3,47 @@ var bg = $('#background');
 
 bg.text(div.html());
 
-var windowHeight = $(document.body).height();
+var windowHeight = $(window).height();
 var contentHeight = div.height();
 
 console.log('Window Height:', windowHeight);
 console.log('Content Height: ', contentHeight);
-console.log('Client Height:', )
+console.log('Client Height:', document.body.clientHeight);
+
+var clientHeight = document.body.clientHeight;
+var backgroundHeight = bg.height();
+var X = (windowHeight - backgroundHeight - 10)/(windowHeight - clientHeight);
 
 $(document).scroll(function(event) {
-	document.getElementById('background').style.top = - 2* document.body.scrollTop;
+	// parallax();
 });
+
+var parallax = function() {
+	console.log(document.body.scrollTop);
+	var scrollTop = document.body.scrollTop;
+	old = Math.floor(X * scrollTop);
+	if(scrollTop < windowHeight - clientHeight + 5) {
+		document.getElementById('background').style.top = old;
+	}
+
+	window.requestAnimationFrame(parallax);
+};
+
+window.requestAnimationFrame(parallax);
+
+/*
+
+Window Height -
+backgroundHeight - client = top
+backgroundHeight - top = client
+backgroundHeight - client*X = 0
+backgroundHeight/X = client
+backgroundHeight/client = X
+
+Input: 0 -> windowHeight - clientHeight;
+Output: 0 -> windowclientHeight - backgroundHeight;
+
+X * (windowHeight - clientHeight) = clientHeight - backgroundHeight;
+
+top -> 0 / clientHeight - backgroundHeight
+*/
