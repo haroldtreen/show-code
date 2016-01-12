ed2670b6eca25db0bd0f3353cfc134320303c1a3
Harold Treen <haroldtreen@gmail.com>
2016-01-12 15:03:33 -0800

    Add gulpfile.js

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,13 +1,27 @@
 var gulp = require('gulp');
-var browserify = require('gulp-browserify');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var cssNano = require('gulp-cssnano');
+var merge = require('gulp-merge');
+var del = require('del');
+
+var packageInfo = require('./package.json');
 
 // Basic usage
 gulp.task('build', function() {
-    // Single entry point to browserify
-    gulp.src(['src/*.js', 'lib/*.js'])
-    .pipe(browserify({
-        transform: ['babelify'],
-        insertGlobals : true
-    }))
-    .pipe(gulp.dest('./build'));
+    var minifiedJs = gulp.src(['src/*.js'])
+    .pipe(uglify())
+    .pipe(rename(`${packageInfo.name}.min.js`));
+
+    var minifiedCss = gulp.src(['src/*.css'])
+    .pipe(cssNano())
+    .pipe(rename(`${packageInfo.name}.min.css`));
+
+    return merge(minifiedJs, minifiedCss).pipe(gulp.dest('./build'));
 });
+
+gulp.task('clean', () => {
+    del('build/*.js');
+});
+
+gulp.task('default', ['clean', 'build']);
